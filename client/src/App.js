8a7355b66c17b08c47d6514a0e315e928d03c7cc
mac <mac@MAC3.local>
2026-02-17 11:59:39 -0500

    Add footer component and styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import HeroBanner from "./components/HeroBanner";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <>
       <Header />
       <HeroBanner />
+      <Footer />
     </>
   );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "../styles/header.css";
 
+
 const Header = () => {
   return (
     <header className="header">
       <div className="header-container">
         <div className="logo">🛒 FreshCart</div>
 
-        <div className="location">📍 Los Angeles</div>
+        <div className="location"> Los Angeles</div>
 
         <div className="search-bar">
           <input type="text" placeholder="Search for products..." />
